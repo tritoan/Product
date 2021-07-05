5a415d04d606fc8c77ef613cdb4813665a5dfe36
tritoan <tritoan1604@gmail.com>
2021-07-05 15:03:12 +0700

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer(props) {
+    return (
+        <div className="footer">
+          <div className="about_product">
+              <span>Giới Thiệu Sản Phẩm</span>
+          </div>
+          <div className="contact">
+              <span>Thông Tin Hỗ Trợ</span>
+          </div>
+            
+        </div>
+    );
+}
+
+export default Footer;
